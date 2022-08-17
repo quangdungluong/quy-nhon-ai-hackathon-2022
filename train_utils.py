cf34cb7775b2291a2020508fab17ad89ae276c22
Luong Quang Dung <luongquangdung00@gmail.com>
2022-08-17 14:35:00 +0700

    [UPDATE] clean and update preprocessing option

--- train_utils.py
+++ train_utils.py
@@ -40,7 +40,6 @@ def train_epoch(model:nn.Module, dataloader:DataLoader, optimizer:Optimizer, sch
     for data in tqdm(dataloader):
         input_ids = data["input_ids"].to(CFG.device)
         attention_mask = data["attention_mask"].to(CFG.device)
-        # targets = targets.type(torch.LongTensor)
         targets = data['target'].to(CFG.device)
         
         optimizer.zero_grad()
@@ -85,7 +84,6 @@ def eval_model(model:nn.Module, dataloader:DataLoader):
     for data in tqdm(dataloader):
         input_ids = data["input_ids"].to(CFG.device)
         attention_mask = data["attention_mask"].to(CFG.device)
-        # targets = targets.type(torch.LongTensor)
         targets = data['target'].to(CFG.device)
         
         outputs = model(input_ids=input_ids, attention_mask=attention_mask)
@@ -106,7 +104,7 @@ def eval_model(model:nn.Module, dataloader:DataLoader):
         final_score += score["competition_score"] / len(score_dict)
     return loss, score_dict, final_score
 
-def train_fold(model_name:str, model_type:str, scheduler_type:str, fold:int, train_fold_df:pd.DataFrame, val_fold_df:pd.DataFrame, oof_file:pd.DataFrame, model_ckpt:str, is_segmented=False, save_last=False) -> pd.DataFrame:
+def train_fold(model_name:str, model_type:str, scheduler_type:str, fold:int, train_fold_df:pd.DataFrame, val_fold_df:pd.DataFrame, oof_file:pd.DataFrame, model_ckpt:str, is_segmented=False, is_preprocessing=True, save_last=False) -> pd.DataFrame:
     """Training 1 fold
 
     Args:
@@ -120,6 +118,7 @@ def train_fold(model_name:str, model_type:str, scheduler_type:str, fold:int, tra
         oof_file (pd.DataFrame): out of fold dataframe
         model_ckpt (str): path to model-checkpoint directory
         is_segmented (bool, optional): is using word segmented or not. Defaults to False.
+        is_preprocessing (bool, optional): is using preprocessing or not. Defaults to True.
         save_last (bool, optional): save last epoch or not. Defaults to False.
 
     Returns:
@@ -134,8 +133,8 @@ def train_fold(model_name:str, model_type:str, scheduler_type:str, fold:int, tra
     tokenizer = create_tokenizer(model_name)
     
     # Create dataloader
-    train_dataloader = create_dataloader(train_fold_df, tokenizer, CFG.batch_size, is_train=True, is_segmented=is_segmented)
-    val_dataloader = create_dataloader(val_fold_df, tokenizer, CFG.batch_size, is_train=False, is_segmented=is_segmented)
+    train_dataloader = create_dataloader(train_fold_df, tokenizer, CFG.batch_size, is_train=True, is_segmented=is_segmented, is_preprocessing=is_preprocessing)
+    val_dataloader = create_dataloader(val_fold_df, tokenizer, CFG.batch_size, is_train=False, is_segmented=is_segmented, is_preprocessing=is_preprocessing)
     
     # Create optimizer, scheduler, loss function
     if CFG.is_llrd : 
@@ -157,14 +156,16 @@ def train_fold(model_name:str, model_type:str, scheduler_type:str, fold:int, tra
         score_report = pd.DataFrame.from_dict(score, orient="index")
         print(f"Train loss: {train_loss:.4f} competition_score: {competition_score:.4f}")
         print(score_report)
+        print()
         # Eval phase
         val_loss, score, competition_score = eval_model(model, val_dataloader)
         score_report = pd.DataFrame.from_dict(score, orient="index")
         print(f"Valid loss: {val_loss:.4f} competition_score: {competition_score:.4f}")
         print(score_report)
+        print()
         # Save best
         if competition_score > best_score:
-            print(f"Improved!!! {best_score:.4f} --> {competition_score:.4f}")
+            print(f"Improved!!! {best_score:.4f} --> {competition_score:.4f}\n")
             best_score = competition_score
             best_epoch = epoch + 1
             save_path = os.path.join(model_ckpt, f'best_model_fold_{fold}.bin')
@@ -202,55 +203,3 @@ def train_fold(model_name:str, model_type:str, scheduler_type:str, fold:int, tra
     torch.cuda.empty_cache()
 
     return oof_file
-    
-# def infer(model_name:str, model_type:str, aspect:str, test_path:str, model_ckpt:str, output_path:str, submission:str):
-#     """Inference
-
-#     Args:
-#         model_name (str): model_name
-#         model_type (str): model_type
-#         aspect (str): aspect for training
-#         test_path (str): path to test_df
-#         model_ckpt (str): path to model-ckpt folder
-#         output_path (str): path to saved folder
-#         submission (str): path to submission file
-#     """
-#     test_df = pd.read_csv(test_path)
-#     tokenizer = create_tokenizer(model_name)
-#     test_dataloader = create_dataloader(test_df, aspect, tokenizer, CFG.batch_size, is_train=False)
-#     logits = []
-#     labels = torch.tensor([])
-#     for file in os.listdir(os.path.join(model_ckpt, aspect)):
-#         path = os.path.join(model_ckpt, aspect, file)
-#         model = create_model(model_name, model_type)
-#         model = model.to(CFG.device)
-#         model.load_state_dict(torch.load(path))
-#         fold_logits = []
-#         labels = torch.tensor([])
-#         model = model.eval()
-
-#         for data, targets in tqdm(test_dataloader):
-#             input_ids = data['input_ids'].to(CFG.device)
-#             attention_mask = data['attention_mask'].to(CFG.device)
-#             targets = targets.type(torch.LongTensor)
-#             targets = targets.to(CFG.device)
-#             labels = torch.cat((labels, targets.detach().cpu()))
-            
-#             outputs = model(input_ids=input_ids, attention_mask=attention_mask)
-#             for out in outputs.detach().cpu().numpy():
-#                 fold_logits.append(out)
-#         logits.append(fold_logits)       
-                 
-#     logits_ = np.array(logits).mean(axis=0)
-#     class_preds = np.argmax(logits_, axis=1)
-
-#     score = {}
-#     score['f1_score'] = get_f1_score(labels, torch.tensor(class_preds))
-#     score['r2_score'] = get_r2_score(labels, torch.tensor(class_preds))
-#     score['competition_score'] = get_score(labels, torch.tensor(class_preds))
-
-#     test_df['label_id'] = class_preds
-#     save_path = os.path.join(output_path, f"infer_test_{aspect}.csv")
-#     test_df.to_csv(save_path, index=False)
-    
-#     print(f"f1_score: {score['f1_score']:.4f} r2_score: {score['r2_score']:.4f} competition_score: {score['competition_score']:.4f}")
